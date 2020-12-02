16383087bba5555bfeabf1f489c974c39f059c95
ThaHolyPaladin <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-12-01 18:31:02 -0800

    Update v1
    an update to allow a ping to be sent to my ec2 instance.

--- Server.py
+++ Server.py
@@ -1,3 +1,10 @@
+# This is the template code for the CNA337 Final Project
+# Michael Horton
+# CNA 337 Fall 2020
+#Worked with Dylan McCormack,  Eric Yevenko
+#Received help from Luma
+
+import os
 class Server:
     """ Server class for representing and manipulating servers. """
 
@@ -7,4 +14,5 @@ class Server:
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        server = os.system("ping -n 4 " + self.server_ip)
+        return self.server_ip
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # This is the template code for the CNA337 Final Project
-# Zachary Rubin, zrubin@rtc.edu
+# Michael Horton
 # CNA 337 Fall 2020
+#Worked with Dylan McCormack,  Eric Yevenko
+#Received help from Luma
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Michael Horton")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    MemeLordSlav = Server('3.21.97.118')
     # TODO - Call Ping method and print the results
+    print(michael-is-a-launch-wizard-"2020".ping())
