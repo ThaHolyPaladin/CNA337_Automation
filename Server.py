b022df895d29e6f6e0db4f4a5d5c99d608c97f24
Mike <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-12-09 13:39:16 -0800

    Update V.01

--- Server.py
+++ Server.py
@@ -2,7 +2,11 @@
 # Michael Horton
 # CNA 337 Fall 2020
 #Worked with Dylan McCormack,  Eric Yevenko
-#Received help from Luma
+#Received help from Luma and Hasan
+# This link helped me with the "Bad Authentication type; allowed types['pubilickey'] Error:
+# https://stackoverflow.com/questions/2224066/how-to-convert-ssh-keypairs-generated-using-puttygen-windows-into-key-pairs-us
+## This website showing how to use paramiko:
+# https://mainlydata.kubadev.com/python/using-paramiko-to-control-an-ec2-instance/
 
 import os
 class Server:
@@ -16,3 +20,26 @@ class Server:
         # TODO - Use os module to ping the server
         server = os.system("ping -n 4 " + self.server_ip)
         return self.server_ip
+
+# NEXT steps are to:  
+# 1.Automate SSH:
+# 2. Connect to my Ec2 Instance using my SSH key
+# 3. Run Update and Upgrade commands in the package manager
+# 4. Disconnect from server
+
+import paramiko
+# create client ssh object so we can use it to connect to the server
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Missing key will be added here #
+
+ssh.connect('3.21.97.118', port=22, username='ubuntu', password='', key_filename='Michael-key.Exported')
+
+stdin, stdout, stderr = ssh.exec_command('''sudo apt-get update
+                                         sudo apt-get upgrade -y
+                                         ''')  # site to force the upgrade: https://itsfoss.com/update-ubuntu/
+stdin.flush()
+data = stdout.read().splitlines()
+for line in data:
+    print(line)
+
+ssh.close()
