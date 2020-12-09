d2734248ddabb792b88e372e232c9297994c1cbd
Mike <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-12-09 13:49:56 -0800

    Update V.01
    adding sources and help to match with Server.py

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 # Michael Horton
 # CNA 337 Fall 2020
 #Worked with Dylan McCormack,  Eric Yevenko
-#Received help from Luma
+#Received help from Luma and Hasan
+## This link helped me with the "Bad Authentication type; allowed types['pubilickey'] Error:
+# https://stackoverflow.com/questions/2224066/how-to-convert-ssh-keypairs-generated-using-puttygen-windows-into-key-pairs-us
+## This website showing how to use paramiko:
+# https://mainlydata.kubadev.com/python/using-paramiko-to-control-an-ec2-instance/
+##https://myarch.com/public-private-key-file-formats
+#This webiste helped me with understanding the file formats
+
 from Server import Server
 
 def print_program_info():
